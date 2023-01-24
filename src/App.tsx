9cb51bc83322e5667ccbac4aa6b219978b4fd8e0
Salim Mouttaki <MOU@inokscapital.com>
2023-01-24 15:51:58 +0100

    removed unused stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import ListComputerScreen from './Pages/ListComputerScreen';
 import CreateComputerScreen from './Pages/CreateComputerScreen';
-import EditComputerScreen from './Pages/EditComputerScreen'
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path='/list-computer' element={<ListComputerScreen/>}/>
         <Route path='/list-users' element={<ListComputerScreen/>}/>
         <Route path='/create-users' element={<ListComputerScreen/>}/>
-        <Route path='/edit-computer/' element={<EditComputerScreen/>}/>
       </Routes> 
     </div>
   );
