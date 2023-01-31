2294fa6ab2f80aa4f74e4b2c25da00a107a0b74c
Salim Mouttaki <MOU@inokscapital.com>
2023-01-31 11:18:11 +0100

    checkbox for computers

--- src/Pages/Computer/ListComputerScreen.tsx
+++ src/Pages/Computer/ListComputerScreen.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AccordionMenu from '../../Components/Accordion'
 import ListComputer from '../../Components/Computers/ListComputer'
 import Navbar from '../../Components/Navbar'
 import { Typography } from '@mui/material'
+import { Checkbox } from '@mui/material'
+import { FormControlLabel } from '@mui/material';
+import '../../css/Details.css'
 
 
 const ListComputerScreen = () => {
+  const [checkbox, setCheckbox] = useState(false)
   return (
     <div>
       <header>
@@ -15,7 +19,8 @@ const ListComputerScreen = () => {
       <div id='accordion'>
         <AccordionMenu />
         <Typography sx={{ fontWeight: 800, fontFamily: 'Gilroy,sans-serif', fontSize: '40px', position: 'absolute', left: '45%', top: '10%', color: '#bd5457' }} className='h1'>List of Computers</Typography>
-        <ListComputer isOnlyFreeChecked={false} /> 
+        <FormControlLabel id='checkbox' control={<Checkbox defaultChecked={false} value={checkbox} onClick={() => setCheckbox(!checkbox)}/>} label="Only in stock" />
+        <ListComputer isOnlyFreeChecked={checkbox} /> 
       </div>
 
     </div>
