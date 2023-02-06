e27dce58327d63f703dba8cc24b3c0112ad92492
Salim Mouttaki <MOU@inokscapital.com>
2023-02-06 15:52:02 +0100

    added stuff

--- src/Components/Computers/DetailsComputer.tsx
+++ src/Components/Computers/DetailsComputer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ComputerDto } from '../Dtos'
 import '../../css/Details.css'
+import { Box } from '@mui/material'
 
 interface DetailsComputerProps {
  computer: ComputerDto
@@ -8,16 +9,17 @@ interface DetailsComputerProps {
 
 const DetailsComputer = (value: DetailsComputerProps) => {
  return (
- <>
-  <div className='details'>Name: {value.computer.name}</div>
-  <div className='details'>Type: {value.computer.type?.type}</div>
-  <div className='details'>Brand: {value.computer.brand}</div>
-  <div className='details'>Processor: {value.computer.processor?.name} {value.computer.processor?.niveau} {value.computer.processor?.vitesse} </div>
-  <div className='details'>Brand: {value.computer.brand}</div>
-  <div className='details'>Ram: {value.computer.ram}Go</div>
-  <div className='details'>State: {value.computer.state?.state}</div>
-  <div className='details'>Comment: {value.computer.comment}</div>
- </>
+  <Box component='div' className='container'>
+   <div className='details'>Name: {value.computer.name}</div>
+   <div className='details'>Type: {value.computer.type?.type}</div>
+   <div className='details'>Brand: {value.computer.brand}</div>
+   <div className='details'>Processor: {value.computer.processor?.name} {value.computer.processor?.niveau} {value.computer.processor?.vitesse} </div>
+   <div className='details'>Brand: {value.computer.brand}</div>
+   <div className='details'>Ram: {value.computer.ram}Go</div>
+   <div className='details'>State: {value.computer.state?.state}</div>
+   <div className='details'>Comment: {value.computer.comment}</div>
+  </Box>
+
 
  )
 }
